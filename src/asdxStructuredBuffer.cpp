67c3d4129f3e6d32e66c64ee9354d873f99d0eaf
Pocol <project.asura@gmail.com>
2021-09-19 09:07:24 +0900

    ビルドエラー修正。

--- src/asdxStructuredBuffer.cpp
+++ src/asdxStructuredBuffer.cpp
@@ -34,7 +34,7 @@ StructuredBuffer::~StructuredBuffer()
 //-------------------------------------------------------------------------------------------------
 //      初期化処理を行います.
 //-------------------------------------------------------------------------------------------------
-bool StructuredBuffer::Init(ID3D11Device* pDevice, uint32_t size, uint32_t stride, const void* pInitData)
+bool StructuredBuffer::Init(ID3D11Device* pDevice, uint32_t size, uint32_t stride, const void* pInitData, bool uav)
 {
     if ( pDevice == nullptr || size == 0 )
     {
@@ -42,9 +42,12 @@ bool StructuredBuffer::Init(ID3D11Device* pDevice, uint32_t size, uint32_t strid
         return false;
     }
 
-    D3D11_BUFFER_DESC desc;
-    ZeroMemory( &desc, sizeof(desc) );
-    desc.BindFlags           = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
+    uint32_t bindFlags = D3D11_BIND_SHADER_RESOURCE;
+    if (uav)
+    { bindFlags |= D3D11_BIND_UNORDERED_ACCESS; }
+
+    D3D11_BUFFER_DESC desc = {};
+    desc.BindFlags           = bindFlags;
     desc.ByteWidth           = size;
     desc.MiscFlags           = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
     desc.StructureByteStride = stride;
@@ -72,8 +75,7 @@ bool StructuredBuffer::Init(ID3D11Device* pDevice, uint32_t size, uint32_t strid
         }
     }
 
-    D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
-    ZeroMemory( &srvDesc, sizeof(srvDesc) );
+    D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
     srvDesc.ViewDimension           = D3D11_SRV_DIMENSION_BUFFEREX;
     srvDesc.Format                  = DXGI_FORMAT_UNKNOWN;
     srvDesc.BufferEx.FirstElement   = 0;
@@ -86,6 +88,23 @@ bool StructuredBuffer::Init(ID3D11Device* pDevice, uint32_t size, uint32_t strid
         return false;
     }
 
+    if (uav)
+    {
+        D3D11_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
+        uavDesc.Format              = DXGI_FORMAT_UNKNOWN;
+        uavDesc.ViewDimension       = D3D11_UAV_DIMENSION_BUFFER;
+        uavDesc.Buffer.FirstElement = 0;
+        uavDesc.Buffer.NumElements  = desc.ByteWidth / desc.StructureByteStride;
+
+        hr = pDevice->CreateUnorderedAccessView( m_Buffer.GetPtr(), &uavDesc, m_UAV.GetAddress() );
+        if (FAILED(hr))
+        {
+            ELOG("Error : ID3D11Device::CreateUnorderedAccessView() Failed.");
+            return false;
+        }
+    }
+
+
     return true;
 }
 
@@ -95,6 +114,7 @@ bool StructuredBuffer::Init(ID3D11Device* pDevice, uint32_t size, uint32_t strid
 void StructuredBuffer::Term()
 {
     m_SRV.Reset();
+    m_UAV.Reset();
     m_Buffer.Reset();
 }
 
@@ -107,8 +127,14 @@ ID3D11Buffer* const StructuredBuffer::GetBuffer() const
 //-------------------------------------------------------------------------------------------------
 //      シェーダリソースビューを取得します.
 //-------------------------------------------------------------------------------------------------
-ID3D11ShaderResourceView* const StructuredBuffer::GetShaderResource() const
+ID3D11ShaderResourceView* const StructuredBuffer::GetSRV() const
 { return m_SRV.GetPtr(); }
+
+//-------------------------------------------------------------------------------------------------
+//      アンオーダードアクセスビューを取得します.
+//-------------------------------------------------------------------------------------------------
+ID3D11UnorderedAccessView* const StructuredBuffer::GetUAV() const
+{ return m_UAV.GetPtr(); }
 
 //-------------------------------------------------------------------------------------------------
 //      アロー演算子です.
