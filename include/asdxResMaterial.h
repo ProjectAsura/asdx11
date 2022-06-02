b66df0c0a0258b3c211e92d371d09b866a83e908
Pocol <project.asura@gmail.com>
2022-06-02 21:13:53 +0900

    コード整理。

--- include/asdxResMaterial.h
+++ include/asdxResMaterial.h
@@ -16,6 +16,20 @@
 
 namespace asdx {
 
+//-----------------------------------------------------------------------------
+// Constant Values.
+//-----------------------------------------------------------------------------
+static const char* TAG_DIFFUSE      = u8"Diffuse";
+static const char* TAG_SPECULAR     = u8"Specular";
+static const char* TAG_BASE_COLOR   = u8"BaseColor";
+static const char* TAG_NORMAL       = u8"Normal";
+static const char* TAG_ORM          = u8"ORM";
+static const char* TAG_OCCLUSION    = u8"Occlusion";
+static const char* TAG_ROUGHNESS    = u8"Roughness";
+static const char* TAG_METALNESS    = u8"Metalness";
+static const char* TAG_EMISSIVE     = u8"Emissive";
+
+
 ///////////////////////////////////////////////////////////////////////////////
 // PROPERTY_TYPE enum
 ///////////////////////////////////////////////////////////////////////////////
