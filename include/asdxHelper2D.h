1e6edb60f9a9455d4637c6e324ba291092da5fad
Pocol <project.asura@gmail.com>
2022-12-22 23:34:49 +0900

    2次元ヘルパー関数とライトを追加。

---
+++ include/asdxHelper2D.h
@@ -0,0 +1,189 @@
+﻿//-----------------------------------------------------------------------------
+// File : asdxHelper2D.h
+// Desc : 2D Helper.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+#pragma once
+
+namespace asdx {
+
+///////////////////////////////////////////////////////////////////////////////
+// DIR2D_TYPE enum
+///////////////////////////////////////////////////////////////////////////////
+enum DIR2D_TYPE
+{
+    DIR2D_NONE,   // 方向無し.
+    DIR2D_LEFT,   // 左.
+    DIR2D_RIGHT,  // 右.
+    DIR2D_UP,     // 上.
+    DIR2D_DOWN,   // 下.
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Int2 structure
+///////////////////////////////////////////////////////////////////////////////
+struct Int2
+{
+    int x = 0;
+    int y = 0;
+
+    Int2() = default;
+
+    Int2(int nx, int ny)
+    : x(nx), y(ny)
+    { /* DO_NOTHING */ }
+
+    Int2 operator + (const Int2& value) const
+    { return Int2(x + value.x, y + value.y); }
+
+    Int2 operator - (const Int2& value) const
+    { return Int2(x - value.x, y - value.y); }
+
+    Int2 operator * (const Int2& value) const
+    { return Int2(x * value.x, y * value.y); }
+
+    Int2 operator / (const Int2& value) const
+    { return Int2(x / value.x, y / value.y); }
+
+    Int2 operator + (int value) const
+    { return Int2(x + value, y + value); }
+
+    Int2 operator - (int value) const
+    { return Int2(x - value, y - value); }
+
+    Int2 operator * (int value) const
+    { return Int2(x * value, y * value); }
+
+    Int2 operator / (int value) const
+    { return Int2(x / value, y / value); }
+
+    Int2& operator += (const Int2& value)
+    {
+        x += value.x;
+        y += value.y;
+        return *this;
+    }
+
+    Int2& operator -= (const Int2& value)
+    {
+        x -= value.x;
+        y -= value.y;
+        return *this;
+    }
+
+    Int2& operator *= (const Int2& value)
+    {
+        x *= value.x;
+        y *= value.y;
+        return *this;
+    }
+
+    Int2& operator /= (const Int2& value)
+    {
+        x /= value.x;
+        y /= value.y;
+        return *this;
+    }
+
+    Int2& operator += (int value)
+    {
+        x += value;
+        y += value;
+        return *this;
+    }
+
+    Int2& operator -= (int value)
+    {
+        x -= value;
+        y -= value;
+        return *this;
+    }
+
+    Int2& operator *= (int value)
+    {
+        x *= value;
+        y *= value;
+        return *this;
+    }
+
+    Int2& operator /= (int value)
+    {
+        x /= value;
+        y /= value;
+        return *this;
+    }
+
+    static Int2 Max(const Int2& lhs, const Int2& rhs)
+    {
+        return Int2(
+            (lhs.x > rhs.x) ? lhs.x : rhs.x,
+            (lhs.y > rhs.y) ? lhs.y : rhs.y);
+    }
+
+    static Int2 Min(const Int2& lhs, const Int2& rhs)
+    {
+        return Int2(
+            (lhs.x < rhs.x) ? lhs.x : rhs.x,
+            (lhs.y < rhs.y) ? lhs.y : rhs.y);
+    }
+
+    static Int2 Clamp(const Int2& value, const Int2& mini, const Int2& maxi)
+    { return Max(mini, Min(maxi, value)); }
+
+    static Int2 GetDir(DIR2D_TYPE type)
+    {
+        static const Int2 kDirection[] = {
+            Int2( 0,  0),   // DIR2D_NONE
+            Int2(-1,  0),   // DIR2D_LEFT
+            Int2( 1,  0),   // DIR2D_RIGHT
+            Int2( 0,  1),   // DIR2D_UP
+            Int2( 0, -1)    // DIR2D_DOWN
+        };
+        return kDirection[type];
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Box2 structure
+//////////////////////////////////////////////////////////////////////////////
+struct Box2
+{
+    Int2 pos;   // 左上原点.
+    Int2 size;  // サイズ.
+
+    Box2() = default;
+
+    Box2(int x, int y, int w, int h)
+    : pos (x, y)
+    , size(w, h)
+    { /* DO_NOTHING */ }
+
+    Box2(const Int2& p, const Int2& s)
+    : pos(p)
+    , size(s)
+    { /* DO_NOTHING */ }
+
+    static Box2 Merge(const Box2& lhs, const Box2& rhs)
+    {
+        return Box2(
+            Int2::Min(lhs.pos,  rhs.pos),
+            Int2::Max(lhs.size, rhs.size));
+    }
+
+    static bool Contains(const Box2& lhs, const Box2& rhs)
+    {
+        return lhs.pos.x < (rhs.pos.x + rhs.size.x)
+            && rhs.pos.x < (lhs.pos.x + lhs.size.x)
+            && lhs.pos.y < (rhs.pos.y + rhs.size.y)
+            && rhs.pos.y < (lhs.pos.y + lhs.size.y);
+    }
+
+    static bool Contains(const Int2& point, const Box2& box)
+    {
+        return (box.pos.x <= point.x && point.x <= (box.pos.x + box.size.x))
+            && (box.pos.y <= point.y && point.y <= (box.pos.y + box.size.y));
+    }
+};
+
+} // namespace asdx
+
---
+++ include/asdxLight.h
@@ -0,0 +1,252 @@
+﻿//-----------------------------------------------------------------------------
+// File : asdxLight.h
+// Desc : Light Data.
+// Copyright(c) Project Asura.
+//-----------------------------------------------------------------------------
+#pragma once
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <asdxMath.h>
+
+
+namespace asdx {
+
+///////////////////////////////////////////////////////////////////////////////
+// DirectionalLight class
+///////////////////////////////////////////////////////////////////////////////
+class DirectionalLight
+{
+public:
+    DirectionalLight() = default;
+
+    DirectionalLight(const Vector3& dir, const Vector3& color, float intensity = 1.0f)
+    : m_Direction(dir)
+    , m_Color(color)
+    , m_Intensity(intensity)
+    { /* DO_NOTHING */ }
+
+    void SetDirection(const Vector3& value)
+    { m_Direction = value; }
+
+    void SetColor(const Vector3& value)
+    { m_Color = value; }
+
+    void SetIntensity(float value)
+    { m_Intensity = value; }
+
+    const Vector3& GetDirection() const
+    { return m_Direction; }
+
+    const Vector3 GetLightVector() const
+    { return -m_Direction; }
+
+    const Vector3& GetColor() const
+    { return m_Color; }
+
+    float GetIntensity() const
+    { return m_Intensity; }
+
+private:
+    Vector3 m_Direction = Vector3(0.0f, -1.0f, 0.0f);   // 照射方向.
+    Vector3 m_Color     = Vector3(1.0f, 1.0f, 1.0f);    // ライトカラー.
+    float   m_Intensity = 1.0f;                         // 強度.
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// PointLight class
+///////////////////////////////////////////////////////////////////////////////
+class PointLight
+{
+public:
+    PointLight() = default;
+
+    PointLight(const Vector3& center, float radius, const Vector3& color,float intensity = 1.0f)
+        : m_Center(center)
+        , m_Radius(radius)
+        , m_Color(color)
+        , m_Intensity(intensity)
+    { /* DO_NOTHING */ }
+
+    void SetCenter(const Vector3& value)
+    { m_Center = value; }
+
+    void SetRadius(float value)
+    { m_Radius = value; }
+
+    void SetColor(const Vector3& value)
+    { m_Color = value; }
+
+    void SetIntensity(float value)
+    { m_Intensity = value; }
+
+    const Vector3& GetCenter() const
+    { return m_Center; }
+
+    float GetRadius() const
+    { return m_Radius; }
+
+    const Vector3& GetColor() const
+    { return m_Color; }
+
+    float GetIntensity() const
+    { return m_Intensity; }
+
+    // 点の包含.
+    bool Contains(const Vector3& point) const
+    {
+        auto d2 = Vector3::DistanceSq(point, m_Center);
+        auto r2 = m_Radius * m_Radius;
+        return (d2 <= r2);
+    }
+
+    // BoundingBoxの包含.
+    bool Contains(const Vector3& mini, const Vector3& maxi) const
+    {
+        auto p  = Vector3::Max(mini, Vector3::Min(m_Center, maxi));
+        auto d2 = Vector3::DistanceSq(p, m_Center);
+        auto r2 = m_Radius * m_Radius;
+        return (d2 <= r2);
+    }
+
+    // BoundingSphereの包含.
+    bool Contains(const Vector3& center, float radius) const
+    {
+        auto d2 = Vector3::DistanceSq(center, m_Center);
+        auto r2 = (m_Radius + radius) * (m_Radius + radius);
+        return (d2 <= r2);
+    }
+
+private:
+    Vector3     m_Center;       // 中心座標.
+    float       m_Radius;       // 半径.
+    Vector3     m_Color;        // ライトカラー.
+    float       m_Intensity;    // 強度.
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// SpotLight class
+///////////////////////////////////////////////////////////////////////////////
+class SpotLight
+{
+public:
+    SpotLight() = default;
+
+    SpotLight(
+        const Vector3&  position,
+        const Vector3&  forward,
+        float           radius,
+        float           innerAngle,
+        float           outerAngle,
+        const Vector3&  color,
+        float           intensity = 1.0f)
+    : m_Position(position)
+    , m_Forward(forward)
+    , m_Radius(radius)
+    , m_InnerAngle(innerAngle)
+    , m_OuterAngle(outerAngle)
+    , m_Color(color)
+    , m_Intensity(intensity)
+    { /* DO_NOTHING */ }
+
+    void SetPosition(const Vector3& value)
+    { m_Position = value; }
+
+    void SetForward(const Vector3& value)
+    { m_Forward = value; }
+
+    void SetRadius(float value)
+    { m_Radius = value; }
+
+    void SetInnerAngle(float value)
+    { m_InnerAngle = value; }
+
+    void SetOuterAngle(float value)
+    { m_OuterAngle = value; }
+
+    void SetColor(const Vector3& value)
+    { m_Color = value; }
+
+    void SetIntensity(float value)
+    { m_Intensity = value; }
+
+    const Vector3& GetPosition() const
+    { return m_Position; }
+
+    const Vector3& GetForward() const
+    { return m_Forward; }
+
+    Vector3 GetLightVector() const
+    { return -m_Forward; }
+
+    float GetRadius() const
+    { return m_Radius; }
+
+    float GetInnerAngle() const
+    { return m_InnerAngle; }
+
+    float GetOuterAngle() const
+    { return m_OuterAngle; }
+
+    const Vector3& GetColor() const
+    { return m_Color; }
+
+    float GetIntensity() const
+    { return m_Intensity; }
+
+    // 点の包含.
+    bool Contains(const Vector3& point) const
+    {
+        auto v = point - m_Position;
+        auto lenSq = Vector3::Dot(v, v);
+        auto v1Len = Vector3::Dot(v, m_Forward);
+        auto distClosestPoint = cos(m_OuterAngle) * sqrt(lenSq - v1Len * v1Len) - v1Len * sin(m_OuterAngle);
+
+        auto angleCull = distClosestPoint > 0.0f;
+        auto frontCull = v1Len > m_Radius;
+        auto backCull  = v1Len < 0.0f;
+        return !(angleCull || frontCull || backCull);
+    }
+
+    // BoundingBoxの包含.
+    bool Contains(const Vector3& mini, const Vector3& maxi) const
+    {
+        auto center = (maxi + mini) * 0.5f;
+        auto extent = (maxi - mini) * 0.5f;
+        auto r = Vector3::Dot(extent, extent);
+        auto v = center - m_Position;
+        auto lenSq = Vector3::Dot(v, v);
+        auto v1Len = Vector3::Dot(v, m_Forward);
+        auto distClosestPoint = cos(m_OuterAngle) * sqrt(lenSq - v1Len * v1Len) - v1Len * sin(m_OuterAngle);
+        auto angleCull = distClosestPoint > r;
+        auto frontCull = v1Len > (r + m_Radius);
+        auto backCull  = v1Len < -r;
+        return !(angleCull || frontCull || backCull);
+    }
+
+    // BoundingSphereの包含.
+    bool Contains(const Vector3& center, float radius) const
+    {
+        auto v = center - m_Position;
+        auto lenSq = Vector3::Dot(v, v);
+        auto v1Len = Vector3::Dot(v, m_Forward);
+        auto distClosestPoint = cos(m_OuterAngle) * sqrt(lenSq - v1Len * v1Len) - v1Len * sin(m_OuterAngle);
+
+        auto angleCull = distClosestPoint > radius;
+        auto frontCull = v1Len > (radius + m_Radius);
+        auto backCull  = v1Len < -radius;
+        return !(angleCull || frontCull || backCull);
+    }
+
+private:
+    Vector3     m_Position;     // 位置座標.
+    float       m_Radius;       // 半径.
+    Vector3     m_Forward;      // 照射方向.
+    float       m_InnerAngle;   // 内角(rad)
+    float       m_OuterAngle;   // 外角(rad)
+    Vector3     m_Color;        // ライトカラー.
+    float       m_Intensity;    // 強度.
+};
+
+} // namespace asdx
